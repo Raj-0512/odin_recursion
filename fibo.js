b2f0109407b092a4b289b6eb76b0ce055599638b
Raj-0512 <rajmanisha1222@gmail.com>
2025-11-05 01:13:58 +0530

    Final Version

--- fibo.js
+++ fibo.js
@@ -1,1 +1,23 @@
+let nums = [];
 
+function fibo(n,nums)
+{
+	if(n < 2)
+	{
+		nums[n] = n;
+		return n;
+	}
+	else if (nums[n] !== undefined) 
+	{
+        return nums[n];
+    }
+	else 
+	{
+		const val = fibo(n-1 , nums) + fibo(n-2 , nums);
+		nums[n] = val;
+		return val;
+	}
+}
+
+fibo(5,nums);
+console.log(nums);
--- merge_sort.js
+++ merge_sort.js
@@ -1,1 +1,67 @@
+let res = [];
+let nums = [6,3,5,8,4,2];
 
+function mergeSort(lb , ub , nums)
+{
+    if(ub > lb)
+    {
+        let mid = Math.floor((lb + ub) / 2);
+
+    	mergeSort(lb , mid , nums);
+    	mergeSort(mid+1 , ub , nums);
+    	merge(lb , mid , ub , nums);
+    }
+    return nums;
+}
+
+function merge(lb , mid , ub , nums)
+{
+	let i = lb;
+	let j = mid + 1;
+	let k = lb;
+
+	while(i <= mid && j <= ub)
+	{
+		if(nums[i] < nums[j])
+		{
+			res[k] = nums[i];
+			i++;
+		}
+		else
+		{
+			res[k] = nums[j];
+			j++;
+		}
+		k++;
+	}
+
+	if(i > mid)
+	{
+        while(j <= ub)
+        {
+        	res[k] = nums[j];
+        	j++;
+        	k++;
+        }
+	}
+	else
+	{
+		while(i <= mid)
+		{
+			res[k] = nums[i];
+			i++;
+			k++;
+		}
+
+	}
+
+	let a = 0;
+
+	for(const num of res)
+	{
+		nums[a] = res[a];
+		a++;
+	}
+}
+
+console.log(mergeSort(0 , nums.length - 1 , nums));
